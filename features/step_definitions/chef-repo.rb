a82658e830c94d60275f782458b958ef93f24cc9
James FitzGibbon <james.i.fitzgibbon@nordstrom.com>
2015-02-06 13:16:10 -0800

    Extend Aruba tests to expose github issue 43

--- features/step_definitions/chef-repo.rb
+++ features/step_definitions/chef-repo.rb
@@ -1,4 +1,4 @@
-Given /^a local mode chef repo with nodes '(.+)'$/ do |nodelist|
+Given(/^a local mode chef repo with nodes '(.+)'$/) do |nodelist|
   # create the repo directory hierarchy
   %w(cookbooks clients nodes data_bags).each do |dir|
     create_dir dir
@@ -11,7 +11,7 @@ chef_zero.enabled true
 EOF
   # create the admin user and capture its private key
   in_current_dir do
-      system 'knife client create admin -z -d -a -c knife.rb > admin.pem'
+    create_admin
   end
   # add the admin key to the knife configuration
   append_to_file 'knife.rb', <<EOF
@@ -20,14 +20,40 @@ client_key 'admin.pem'
 EOF
   # create the requested nodes
   nodelist.split(/,/).each do |node|
-    run_simple "knife client create #{node} -z -d -c knife.rb"
-    run_simple "knife node create #{node} -z -d -c knife.rb"
+    in_current_dir do
+      create_client(node)
+      create_node(node)
+    end
   end
 end
 
-When /^I delete clients? '(.+)' from the Chef server$/ do |nodelist|
+Given(/^I delete clients? '(.+)' from the Chef server$/) do |nodelist|
   nodelist.split(/,/).each do |node|
-    run_simple "knife client delete #{node} -z -d -y -c knife.rb"
+    in_current_dir do
+      delete_client(node)
+    end
   end
 end
 
+Given(/^I regenerate the client key for the node '(.+)'$/) do |node|
+  in_current_dir do
+    delete_client(node)
+    create_client(node)
+  end
+end
+
+def create_node(name)
+  system "knife node create #{name} -z -d -c knife.rb"
+end
+
+def create_admin
+  create_client('admin', '-a')
+end
+
+def create_client(name, args = nil)
+  system "knife client create #{name} -z -d -c knife.rb #{args} > #{name}.pem"
+end
+
+def delete_client(name)
+  system "knife client delete #{name} -y -z"
+end
