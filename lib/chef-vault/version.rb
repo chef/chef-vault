dfb2e3f19554044a9ae6edd4e7bb1b1704ea414f
Thom May <thom@chef.io>
2016-01-25 15:46:44 +0000

    prep for 2.7.0

--- lib/chef-vault/version.rb
+++ lib/chef-vault/version.rb
@@ -1,5 +1,6 @@
 # Description: ChefVault VERSION file
 # Copyright 2013-15, Nordstrom, Inc.
+# Copyright 2015-2016, Chef Software, Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +15,6 @@
 # limitations under the License.
 
 class ChefVault
-  VERSION = "2.6.1"
+  VERSION = "2.7.0"
   MAJOR, MINOR, TINY = VERSION.split(".")
 end
