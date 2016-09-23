6ba4bdc397c520d7d46a8c9d2468e20712c638e8
Xavier Krantz <x.krantz@criteo.com>
2016-09-23 15:29:48 +0200

    Display a more comprehensive message from Vault context when Chef-server REST API returns an error

--- spec/chef-vault/item_spec.rb
+++ spec/chef-vault/item_spec.rb
@@ -282,6 +282,40 @@ RSpec.describe ChefVault::Item do
       @vaultitem.clients(client)
       expect(@vaultitem.clients).to include("foo")
     end
+
+    # To prevent
+    it "should not blow up when it gets an error while trying to get the client info" do
+      keys = double("keys",
+                    search_query: "*:*",
+                    add: nil,
+                    admins: [],
+                    clients: ["testnode"])
+      allow(keys).to receive(:[]).with("id").and_return("bar_keys")
+      allow(ChefVault::ItemKeys).to receive(:new).and_return(keys)
+
+      item = ChefVault::Item.new("foo", "bar")
+
+      # Fake the query
+      node  = double("node", name: "testnode")
+      query = double("query")
+      allow(Chef::Search::Query).to receive(:new).and_return(query)
+      allow(query).to receive(:search).and_yield(node)
+
+      # Fake the Error from the remote Chef-server
+      http_response = double("http error from Chef-server")
+      http_error    = Net::HTTPServerException.new("ERROR", http_response)
+      allow(http_response).to receive(:code).and_return("403")
+
+      allow(ChefVault::ChefPatch::ApiClient)
+        .to receive(:load)
+        .with("testnode")
+        .and_raise(http_error)
+
+      expect { item.clients("*:*") }.not_to raise_error
+      expect { item.clients("*:*") }
+        .to output(/Something went wrong with client 'testnode'; skipping/).to_stdout
+    end
+
   end
 
   describe "#admins" do
