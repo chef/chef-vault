697ed571c742287d1196ff84e701c8cb1da43698
Grégoire Seux <g.seux@criteo.com>
2017-10-21 18:16:49 +0200

    Correct style errors
    Change-Id: Id57933fc9d8a8393d4fd30874610033229ec5a4e

--- features/step_definitions/chef-databag.rb
+++ features/step_definitions/chef-databag.rb
@@ -1,4 +1,4 @@
-When /^I create a data bag '(.+)' containing the JSON '(.+)'$/ do |bag, json|
+When(/^I create a data bag '(.+)' containing the JSON '(.+)'$/) do |bag, json|
   write_file "item.json", json
   run_simple "knife data bag create #{bag} -z -c knife.rb -d"
   run_simple "knife data bag from_file #{bag} -z -c knife.rb item.json"
