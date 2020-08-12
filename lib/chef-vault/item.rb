9162162679d095e47720cca3c9b2acc6f2f2a862
Tim Smith <tsmith84@gmail.com>
2020-08-12 14:16:47 -0700

    Optimize requires for non-omnibus installs
    require is quite slow in Ruby and doing requires for things you've already required is also slow. We've used this simple hack in Chef to speed up our requires. In the omnibus installs we patch how rubygems works to make this somewhat pointless, but this will help non-omnibus installs
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef-vault/item.rb
+++ lib/chef-vault/item.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "securerandom"
+require "securerandom" unless defined?(SecureRandom)
 require_relative "mixins"
 
 class ChefVault
