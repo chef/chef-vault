f9f8f5b4867cb98e3ea8919a9c1e82494089c585
nikhil2611 <nikhilgupta2102@gmail.com>
2023-09-20 11:20:28 +0530

    update some gems
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,8 +1,13 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
+
 # This will run ruby test on windows platform
 
 Write-Output "--- Bundle install"
+ruby --version
+bundler --version
+gem update --system
+If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
 bundle config --local path vendor/bundle
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
