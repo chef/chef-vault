3ab5ac11d3d62a6bb4a40468a0022ffb8e0994b9
nikhil2611 <nikhilgupta2102@gmail.com>
2025-03-20 13:11:15 +0530

    trying to fix timeout
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- features/step_definitions/chef-repo.rb
+++ features/step_definitions/chef-repo.rb
@@ -91,23 +91,16 @@ end
 def create_client(name)
   pem_file = "#{name}.pem"
   command = "knife client create #{name} -z -d -c config.rb"
-  timeout_seconds = 40
+  timeout_seconds = 30
   begin
     with_environment("ARUBA_TIMEOUT" => timeout_seconds.to_s) do
       run_command_and_stop(command, timeout: timeout_seconds)
     end
-    attempts = 0
     pem_content = last_command_started.stdout.strip
 
-    while pem_content.empty? && attempts < 3
-      puts "⏳ Waiting for .pem content... Attempt #{attempts + 1}/5"
+    if platform?("windows") && pem_content.empty?
       sleep 1
       pem_content = last_command_started.stdout.strip
-      attempts += 1
-    end
-
-    if pem_content.empty?
-      raise "❌ pem_content is still empty after waiting."
     end
 
     File.write(pem_file, pem_content)
