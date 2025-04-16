8281012ed9f18b6be33f45dec495afbe74829183
nikhil2611 <nikhilgupta2102@gmail.com>
2025-04-16 20:58:58 +0530

    trying out to fix the pipeline error
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require "aruba/cucumber"
 
 Aruba.configure do |config|
-  config.exit_timeout = 30
+  config.exit_timeout = 15
   config.io_wait_timeout = 2
   config.activate_announcer_on_command_failure = %i{stderr stdout command}
 end
