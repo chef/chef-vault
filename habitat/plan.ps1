d6fcd16bcdb8a0c88b72ca4f6c5c979234602011
nitin sanghi <nsanghi@progress.com>
2026-02-18 17:47:34 +0530

    Remove git deps in gem spec file
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -67,7 +67,7 @@ function Invoke-Install {
                      "*/JSON-Schema-Test-Suite", "JSON-Schema-Test-Suite")
 
     try {
-        Push-Location $project_root
+        Push-Location $pkg_prefix
         bundle config --local gemfile $project_root/Gemfile
          Write-BuildLine "** generating binstubs for chef-vault with precise version pins"
 	 Write-BuildLine "** generating binstubs for chef-vault with precise version pins $project_root $pkg_prefix/bin " 
